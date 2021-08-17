e9ca17ed28ca6424c5b9a56295977269e35383b8
dmvillasenor <46989848+dmvillasenor@users.noreply.github.com>
2021-08-17 14:23:34 -0700

    Orbit Test

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from ursina.shaders import *
 from object import Object
 
 app = Ursina()
-star = Object('sphere', color.blue, 1, (0.1, 0, 0), normals_shader)
-star2 = Object('sphere', color.red, 2, (5, 0, 3), basic_lighting_shader)
+star = Object('sphere', color.blue, 1000000000000000, (10, 10, 10), (0, 0, 0), normals_shader, 0)
+star2 = Object('sphere', color.red, 1, (10, 10, 10), (50, 0, 0), basic_lighting_shader, (0, 20, 20))
 
 
 def main():
@@ -15,7 +15,12 @@ def main():
 
 
 def update():
-    star.accel_update()
+    star2.update_position(time.dt)
+    (radius, rel_pos) = star2.calc_radius((star.x, star.y, star.z))
+    accel_vector = star2.calc_accel(radius, star.mass, rel_pos)
+    star2.accel_velocity(accel_vector, time.dt)
+
+    cube = Object('cube', color.blue, 1, (0.5, 0.5, 0.5), (star2.x, star2.y, star2.z), basic_lighting_shader, 0)
 
 
 def input(key):
--- object.py
+++ object.py
@@ -1,11 +1,49 @@
 from ursina import Entity, Shader
 import scipy.constants
+from numpy import sin, cos, arctan, sqrt, subtract, hypot
 
 
 class Object(Entity):
-    def __init__(self, model, color, mass, position, shader):
-        Entity.__init__(self, model=model, color=color, scale=mass, position=position, shader=shader)
+    def __init__(self, model, color, mass, scale, position, shader, velocity):
+        Entity.__init__(self, model=model, color=color, scale=scale, position=position, shader=shader)
         self.mass = mass
+        self.velocity = velocity
 
-    def accel_update(self):
+    def calc_radius(self, parent_position):
+        (temp_x, temp_y, temp_z) = tuple(subtract(self.position, parent_position))
+        xy = hypot(temp_x, temp_y)
+        radius = hypot(xy, temp_z)
+        rel_pos = (temp_x, temp_y, temp_z)
+        return radius, rel_pos
+
+    def calc_accel(self, radius, parent_mass, rel_pos):
+        temp_x, temp_y, temp_z = rel_pos
+        xz_angle = arctan(temp_z / temp_x)
+
+        xz = hypot(temp_x, temp_z)
+        y_angle = arctan(temp_y / xz)
+
+        if temp_x < 0:
+            xz_angle += scipy.constants.pi
+
         G = scipy.constants.gravitational_constant
+        Force = (G * parent_mass * self.mass) / (radius * radius)
+        acceleration = Force / self.mass
+
+        accel_y = acceleration * sin(y_angle)
+        accel_xy = acceleration * cos(y_angle)
+        accel_z = accel_xy * sin(xz_angle)
+        accel_x = accel_xy * cos(xz_angle)
+
+        accel_vector = (accel_x, accel_y, accel_z)
+        return accel_vector
+
+    def accel_velocity(self, accel_vector, time_pass):
+        accel_vector = tuple([z * time_pass for z in accel_vector])
+        self.velocity = tuple(subtract(self.velocity, accel_vector))
+
+    def update_position(self, time):
+        (x, y, z) = self.velocity
+        self.x += (x * time)
+        self.y += (y * time)
+        self.z += (z * time)
